509dbb9ed367f807997341e089c6151d61314f5c
alidavoodimoghadam <alidavid797@gmail.com>
2024-11-16 20:33:31 -0800

    setting up all the api requests

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const app = new express();
+
+var genres = [
+    'action',
+    'comedy',
+    'romance',
+    'SciFi'
+]
+
+app.get('/api/genres', (req, res)=>{
+    res.send(genres);
+});
+
+app.put('/api/genres', (req, res) => {
+    const newGenre = req.query.genre;
+
+    if(newGenre){
+        genres.push(newGenre)
+        res.send(genres);
+    } else {
+        res.status(400).send({ error: 'Genre is not stated'});
+    }
+})
+
+app.delete('/api/genres', (req, res) =>{
+    const removeGenre = req.query.genre;
+
+    if(removeGenre){
+        const index = genres.indexOf(removeGenre);
+        if (index > -1) {
+            genres.splice(index, 1); // Remove the element at the found index
+          }
+        res.send(genres)
+    }
+})
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`Listening on Port ${port}`)
+});
